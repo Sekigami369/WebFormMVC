c15a46e4e0369776b395b67aaf1ac1b162728331
sekigmai369 <skgmyum@gmail.com>
2023-11-08 21:48:56 +0900

    first commit

---
+++ Models/MyDatabaseDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebFormMVC.Models
+{
+    public class MyDatabaseDbContext : DbContext
+    {
+        public DbSet<PersonalDataModel> personalDatas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
---
+++ Models/PersonalDataModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebFormMVC.Models
+{
+    public class PersonalDataModel
+    {
+        [Column("InputID")]
+        [Required]
+        public int InputID { get; set; }
+
+        [Column("Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column("BirthDay")]
+        [Required]
+        public DateTime BirthDay { get; set; }
+
+        [Column("Gendrt")]
+        [Required]
+        public  string Gender { get; set; }
+
+        [Column("JobName")]
+        [Required]
+        public string JobName { get; set; }
+
+    }
+}
